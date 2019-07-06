37c9dd38a4e0b98ff1398adb87e3c7d6391d9d9d
xt <32516078+xtzero@users.noreply.github.com>
2019-07-06 18:05:02 +0800

    Update db.php

--- api/lib/db.php
+++ api/lib/db.php
@@ -2,12 +2,12 @@
 class db{
     private $db_host;                                           //数据库域名
     private $db_usr     =           'root';                     //数据库用户名
-    private $db_pwd     =           'QcloudDBlogin950211.';     //数据库密码
+    private $db_pwd     =           'AliyunDBLogin950211..';     //数据库密码
     private $db;                                                //当前数据库链接
     private static $obj =           null;                       //属性值为对象,默认为null
 
     private function  __construct(){
-        $this->db_host = (strpos($_SERVER['HTTP_HOST'],'localhost') !== false)? '118.126.93.163:3306' : 'localhost';
+        $this->db_host = (strpos($_SERVER['HTTP_HOST'],'localhost') !== false)? '47.102.202.86:80:3306' : 'localhost';
         $db = mysqli_connect($this->db_host,$this->db_usr,$this->db_pwd);
         if($db){
             mysqli_select_db($db,'xt');
